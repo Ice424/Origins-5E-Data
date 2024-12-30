40ce824333f291477efe1cc9e4662bb54c342ab3
Ice424 <e4elliotd@gmail.com>
2024-12-30 20:52:34 +0000

    chonky commits

--- particle.py
+++ particle.py
@@ -1,4 +1,4 @@
-file = open("data\\chill\\function\\orginorb.mcfunction", "r")
+file = open("data\\chill\\function\\sprite-0001.mcfunction", "r")
 commands = []
 for line in file:
     commands.append(line.strip().split(" "))
@@ -7,8 +7,7 @@ file.close()
 out = []
 
 for command in commands:
-    out.append(f"particle dust{{color: {str(command[2:5]).replace("'", "")}, scale: 1}} {
-               str(command[6:9])[1:-1].replace("'", "").replace(",", "")} 0 -1 0 10 0 force\n", )
+    out.append(f"particle dust{{color: {str(command[2:5]).replace("'", "")}, scale: 1}} {str(command[6:9])[1:-1].replace("'", "").replace(",", "")} 0 -1 0 10 0 force\n", )
 print(out)
 file = open("data\\chill\\function\\test.mcfunction", "w")
 file.writelines(out)
